f432df3e757f6e64be816c177a0286f7abe3c57f
Developer-Nasim <ajnasim72@gmail.com>
2024-05-02 20:30:09 +0600

    desktop version done

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+(function($) {
+  "use strict";
+  
+ // menu 
+  $('.siteBar-btn').click( function (){ 
+    $('.mobile-menu').toggleClass('siteBar');
+    $('.menuShadow').toggleClass('show');
+  }); 
+
+    
+  // owlCarousel
+  $(".all-feedbacks").owlCarousel({
+    loop: true, 
+    items: 1,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+    nav: false,
+    dots: true,
+    // responsive: {
+    //   0: {
+    //     items: 2
+    //   },
+    //   767: {
+    //     items: 3
+    //   },
+    //   992: {
+    //     items: 6
+    //   }
+    // }
+  });
+
+ 
+  
+ 
+})(jQuery);
