a0886e3042e012cd59f0a2fb1564f7b643eae3b5
Developer-Nasim <ajnasim72@gmail.com>
2024-05-03 18:42:05 +0600

    multistep done 1st

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@
   }); 
 
     
-  // owlCarousel
+  // feedback slider
   $(".all-feedbacks").owlCarousel({
     loop: true, 
     items: 1,
@@ -32,24 +32,109 @@
     // }
   });
 
+
+  // Get started auto-slider
  function AutoPlaySlider() {
-  const topBlk = document.querySelector('.getstarted-wrap')
-  setInterval(() => {
-    if (topBlk.classList.contains('first_step')) {
-      topBlk.classList.remove('first_step')
-      topBlk.classList.add('second_step')
-    }else if (topBlk.classList.contains('second_step')){
-      topBlk.classList.remove('second_step')
-      topBlk.classList.add('third_step')
-    }else if (topBlk.classList.contains('third_step')){
-      topBlk.classList.remove('third_step')
-      topBlk.classList.add('first_step')
-    }else{
-      topBlk.classList.add('first_step')
-    }
-  }, 3000);
+  if (document.querySelectorAll('.getstarted-wrap').length > 0) { 
+    const topBlk = document.querySelector('.getstarted-wrap')
+    setInterval(() => {
+      if (topBlk.classList.contains('first_step')) {
+        topBlk.classList.remove('first_step')
+        topBlk.classList.add('second_step')
+      }else if (topBlk.classList.contains('second_step')){
+        topBlk.classList.remove('second_step')
+        topBlk.classList.add('third_step')
+      }else if (topBlk.classList.contains('third_step')){
+        topBlk.classList.remove('third_step')
+        topBlk.classList.add('first_step')
+      }else{
+        topBlk.classList.add('first_step')
+      }
+    }, 3000);
+  }
  }
  AutoPlaySlider()
+ 
+ 
+//  Multistep forms
+function MultistepForm() {
+  if (document.querySelectorAll(".all_form_step").length > 0) {
+    const form = document.querySelector('form.multistep')
+    let activeItem = form.querySelector('.all_form_step > .active')
+    const nextBtn = form.querySelector('.step_fixed_bottom button')
   
+    nextBtn.addEventListener('click', () => {
+      if (activeItem.nextElementSibling) {
+        activeItem.classList.remove('active')
+        activeItem.nextElementSibling.classList.add('active')
+
+        if (!activeItem.nextElementSibling.nextElementSibling) {
+          form.classList.add('last_one')
+        }
+      }
+      activeItem = document.querySelector('.all_form_step > .active')
+    })
+    
+
+
+
+    // none of them function
+    const NoneOFTheme = form.querySelector('.dslectall') 
+    NoneOFTheme.addEventListener('click', () => {
+      const AllInp = NoneOFTheme.parentElement.parentElement.parentElement.querySelectorAll("input")
+      AllInp.forEach(inp => {
+        if (!inp.classList.contains('dslectall')) {
+          inp.checked = false 
+        }
+      })
+    })
+
+    // Personal info dependencies
+    const marriedStatus = document.querySelectorAll('input[name="martial-status"]')
+    const rateSpouse = document.querySelector('.rate_spouse')
+    marriedStatus.forEach(mSt => {
+      mSt.addEventListener('change', () => {
+        if (mSt.value == "yes") {
+          rateSpouse.style.display = "block"
+        }else{
+          rateSpouse.style.display = "none"
+        }
+      })
+    })
+
+    const rateSpouseYes = document.querySelectorAll('input[name="rate-status"]')
+    const spouseInfo = document.querySelector('.spouse_info')
+    rateSpouseYes.forEach(mSt => {
+      mSt.addEventListener('change', () => {
+        if (mSt.value == "yes") {
+          spouseInfo.style.display = "block"
+        }else{
+          spouseInfo.style.display = "none"
+        }
+      })
+    })
+
+    const appliedForInsurance = document.querySelectorAll('input[name="applied_insurance"]')
+    const declinedCoverage = document.querySelector('.declined_coverage')
+    appliedForInsurance.forEach(mSt => {
+      mSt.addEventListener('change', () => {
+        if (mSt.value == "yes") {
+          declinedCoverage.style.display = "block"
+        }else{
+          declinedCoverage.style.display = "none"
+        }
+      })
+    })
+
+
+
+
+
+  }
+}
+MultistepForm()
+
+
+
  
 })(jQuery);
